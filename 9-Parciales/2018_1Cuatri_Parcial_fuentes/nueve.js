1d6bcc61b9737a3d516bc6c09d73c1ae07eb4f2f
alumno <alumno>
2020-03-03 19:24:21 -0300

    bleble

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,17 @@
 function mostrar()
 {
+    var marca;
+    var peso;
+    var temperatura;
 
+    var marca = prompt("Ingrese la marca del producto");
+
+    var peso = prompt("Ingrese el peso del producto");
+    while(peso <0 || peso >100){
+
+        peso = prompt("Ingrese un peso valido");
+        
+    }
+        
+    
 }
